301bca981fe967537db82db07a16af4ae2908c47
Felipe Figueroa <amenadiel@gmail.com>
2021-12-17 00:52:40 -0300

    cleaning some deps

--- functions/client_hints.ts
+++ functions/client_hints.ts
@@ -5,5 +5,14 @@ export async function onRequest({ request }: { request: Request }): Promise<Resp
     const vh = Number(request.headers.get('sec-ch-viewport-height')) || 'N/A'
     const dpr = Number(request.headers.get('dpr') || request.headers.get('sec-ch-dpr')) || 'N/A'
     const webp = request.headers.get('accept')?.includes('webp') ? 'yes' : 'nope'
-    return new Response(JSON.stringify({ vw, vh, dpr, webp }), { headers: { 'content-type': 'application/json' } });
+
+    let placeholder = encodeURIComponent(`000?text=Viewport-Height:+${vw}%0AViewport-Width:+${vh}%0ADPR:+${dpr}%0AWebp%20support:+${webp}&font_size=21&font=museo`),
+        requested = `https://resizer.pictures/auto_dpr/fakeimg.pl/300x145/fff/${placeholder}`
+    console.log({ requested })
+    return fetch(requested).then(res => {
+        if (!res.ok) {
+            return new Response(JSON.stringify({ vw, vh, dpr, webp }), { headers: { 'content-type': 'application/json' } });
+        }
+        return res
+    })
 }
